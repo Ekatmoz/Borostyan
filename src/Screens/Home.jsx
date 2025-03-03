497cbb9cd631397596a07e166a4b6757d886f5ca
Ekatmoz <ekatmoz@gmail.com>
2025-03-03 22:13:09 +0100

    added info alert

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,24 +1,25 @@
-//import React, { useEffect } from 'react';
-//import Swal from 'sweetalert2';
+import React, { useEffect } from 'react';
+import Swal from 'sweetalert2';
 import AboutUs from "../Components/About us/AboutUs";
 import Advantages from "../Components/Advantages/Advantages";
 import Delivery from "../Components/Delivery/Delivery";
 import Gallery from "../Components/Gallery/Gallery";
 import Hero from "../Components/Hero/Hero";
-import Information from "../Components/Information/Information";
+import Description from "../Components/Info/Description";
 import Partners from "../Components/Partners/Partners";
 import Products from "../Components/Products/Products";
 
 const Home = () => {
 
-  // useEffect(() => {
-  //   Swal.fire({
-  //     title: 'Kedves vásárlóink!',
-  //     html: 'December 31.én 12:00 ig nyitva vagyunk. <br />Január 1.-Január 5. között zárva vagyunk,ez idő alatt a kiszállítás is szünetel!<br />Kellemes ünnepeket kívánunk!!',
-  //     icon: 'info',
-  //     confirmButtonText: 'Ok',
-  //    });
-  // }, [])
+useEffect(() => {
+   Swal.fire({
+     title: 'Kedves vásárlóink!',
+     html: 'Figyelem‼️ <br />Március 14,15.en zárva leszünk‼️<br />',
+     icon: 'info',
+     confirmButtonText: 'Ok',
+    });
+ }, []);
+
   return (
     <div id="home">
       <Hero/>
@@ -27,7 +28,7 @@ const Home = () => {
       <AboutUs/>
       <Delivery/>
       <Gallery/>
-      <Information/>
+      <Description/>
       <Partners/>
     </div>
   )
