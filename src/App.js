6122743654e5af6b909c99318f66ecfd115da3db
Ekatmoz <ekatmoz@gmail.com>
2025-03-03 22:31:10 +0100

    added scroll top

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './Screens/Home';
-import Info from './Screens/Info';
+import Info from './Screens/Szikviz/Info';
 import IndexNavbar from './Components/Navbar/IndexNavbar';
 import Footer from './Components/Footer/Footer';
+import ScrollToTop from './Components/ScrollToTop';
+import PiViz from './Screens/PiViz/PiViz';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop /> 
       <IndexNavbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/info" element={<Info/>} />
+        <Route path="/szikviz" element={<Info/>} />
+        <Route path="/piviz" element={<PiViz/>} />
       </Routes>
     <Footer/>
     </BrowserRouter>
