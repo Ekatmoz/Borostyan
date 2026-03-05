1d7a837fd1cdeed6c4a68d0ef7c1bd17d989c1a2
Ekatmoz <ekatmoz@gmail.com>
2026-03-05 12:59:54 +0100

    reforming hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,31 +1,42 @@
-import './Hero.css'
+import { memo } from 'react';
+import './Hero.css';
 import bottle from '../../img/bottle.png';
 import { Button, Container, Row, Col } from 'reactstrap';
 
+const BOTTLE_W = 450;
+const BOTTLE_H = Math.round((3911 / 2480) * BOTTLE_W);
+
 const Hero = () => {
   return (
-    <div id='home' className='hero'>
+    <section className='hero' aria-label='Hero'>
       <Container fluid>
-        <Row className='d-flex justify-content-center'>
-          <Col sm={6} className='header d-flex'>
-            <div className='d-flex justify-content-center flex-column align-items-center'>
+        <Row className='justify-content-center align-items-center'>
+          <Col xs={12} md={6} className='header'>
+            <div className='hero__content text-center'>
               <h1>Buborékok Mestere - Borostyán Szikvíz</h1>
               <p>Szeretettel neked</p>
-              <div className='btn'>
-              <Button className='btn-round' color='info' type='button' href='#products'>
-              Termékek
-            </Button>
+              <div className='hero__cta'>
+                <Button className='btn-round' color='info' type='button' href='#products'>
+                  Termékek
+                </Button>
+              </div>
             </div>
-            </div>
-            
           </Col>
-          <Col sm={4}>
-            <img src={bottle} alt='bottle' width='450px' />
+          <Col xs={12} md={4} className='hero__imageCol'>
+            <img
+              className='hero__image'
+              src={bottle}
+              alt='Borostyán Szikvíz palack'
+              width={BOTTLE_W}
+              height={BOTTLE_H}
+              decoding='async'
+              fetchPriority='high'
+            />
           </Col>
         </Row>
       </Container>
-    </div>
+    </section>
   );
 };
 
-export default Hero;
+export default memo(Hero);
